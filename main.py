7aea2eb1aa9951fcdfa1e10d5bbd8d30b1ef54e0
sonu prakash k.s <ssonuprakashks@gmail.com>
2026-02-13 15:44:58 +0530

    su

--- main.py
+++ main.py
@@ -25,3 +25,4 @@ def read_root(request: Request):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+ 
--- models.py
+++ models.py
@@ -1,1 +1,11 @@
 from db import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    address = Column(String) 
+    phone = Column(String)
+    isprime= Column(Boolean)
